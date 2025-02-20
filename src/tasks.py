8bfb244e12558c41a7b0e8d586c8921a1e8922d4
Harmehar Kaur <84430398+Harmehar-kaur@users.noreply.github.com>
2025-02-20 21:15:33 +0530

    Update tasks.py

--- src/tasks.py
+++ src/tasks.py
@@ -20,25 +20,18 @@ from openrelik_worker_common.task_utils import create_task_result, get_input_fil
 from .app import celery
 
 # Task name used to register and route the task to the correct queue.
-TASK_NAME = "your-worker-package-name.tasks.your_task_name"
+TASK_NAME = "openrelik-worker-reg-ripper.tasks.registry"
 
 # Task metadata for registration in the core system.
 TASK_METADATA = {
-    "display_name": "<REPLACE_WITH_NAME_OF_THE_WORKER>",
-    "description": "<REPLACE_WITH_DESCRIPTION_OF_THE_WORKER>",
+    "display_name": "RegRipper",
+    "description": "Registry analysis tool for forensic investigation",
     # Configuration that will be rendered as a web for in the UI, and any data entered
     # by the user will be available to the task function when executing (task_config).
-    "task_config": [
-        {
-            "name": "<REPLACE_WITH_NAME>",
-            "label": "<REPLACE_WITH_LABEL>",
-            "description": "<REPLACE_WITH_DESCRIPTION>",
-            "type": "<REPLACE_WITH_TYPE>",  # Types supported: text, textarea, checkbox
-            "required": False,
-        },
-    ],
+    "task_config": [],
 }
 
+logger = get_task_logger(__name__)
 
 @celery.task(bind=True, name=TASK_NAME, metadata=TASK_METADATA)
 def command(
@@ -63,27 +56,48 @@ def command(
     """
     input_files = get_input_files(pipe_result, input_files or [])
     output_files = []
-    base_command = ["<REPLACE_WITH_COMMAND>"]
-    base_command_string = " ".join(base_command)
 
     for input_file in input_files:
         output_file = create_output_file(
             output_path,
-            display_name=input_file.get("display_name"),
-            extension="<REPLACE_WITH_FILE_EXTENSION>",
-            data_type="<[OPTIONAL]_REPLACE_WITH_DATA_TYPE>",
+            display_name="RegRipper_Output.txt",
+            data_type="openrelik:worker:regripper:txt_log",
         )
-        command = base_command + [input_file.get("path")]
+        command = [
+            "wine",
+            "/opt/regripper/rip.exe",
+            "-r",
+            "",
+            "-f",
+            "sam",
+            "-l",
+            "-u",
+        ]
+    logger.debug("Creating temporary directory for RegRipper processing.")
+    with TemporaryDirectory(dir=output_path) as temp_dir:
+        # Prepare input files
+        logger.debug("Copying input registry files for processing.")
+        for input_file in input_files:
+            filename = os.path.basename(input_file.get("path"))
+            target_path = os.path.join(temp_dir, filename)
+            os.link(input_file.get("path"), target_path)
+            command[2] = target_path  # Set the registry hive file to process
 
-        # Run the command
-        with open(output_file.path, "w") as fh:
-            subprocess.Popen(command, stdout=fh)
+            # Run RegRipper
+            logger.debug("Running RegRipper")
+            progress_update_interval_in_s: Final[int] = 2
+            with open(rip_output.path, "w") as output_file:
+                with subprocess.Popen(command, stdout=output_file) as proc:
+                    logger.debug("Waiting for RegRipper to finish")
+                    while proc.poll() is None:
+                        self.send_event("task-progress", data=None)
+                        time.sleep(progress_update_interval_in_s)
 
-        output_files.append(output_file.to_dict())
-
-    if not output_files:
-        raise RuntimeError("<REPLACE_WITH_ERROR_STRING>")
-
+    # Populate the list of resulting output files.
+    logger.debug("Collecting output files")
+    if os.stat(rip_output.path).st_size > 0:
+        output_files.append(rip_output.to_dict())
+        
     return create_task_result(
         output_files=output_files,
         workflow_id=workflow_id,
